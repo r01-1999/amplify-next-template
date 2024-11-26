bbeebd90fdfb85364d92fe45b38ad7da6cc5b1fb
Rengarajan <renga.anbil@gmail.com>
2024-11-26 11:07:06 +0530

    feat: added a function.
    feat: added a function.

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,13 @@
 import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource.js';
-import { data } from './data/resource.js';
+import { auth } from './auth/resource';
+import { data } from './data/resource';
+import { myFirstFunction } from './my-first-function/resource';
 
+/**
+ * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
+ */
 defineBackend({
   auth,
   data,
+  myFirstFunction
 });
